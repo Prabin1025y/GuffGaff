ba9c3714ceb9a93f08a8ac5674273a7c861af491
Prabin1025y <acharyaprabin1025y@gmail.com>
2024-11-09 22:53:04 +0545

    debug -2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthContextProvider } from './context/AuthContext.jsx'
 import { SocketContextProvider } from './context/SocketContext.jsx'
+import { AuthContextProvider } from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
